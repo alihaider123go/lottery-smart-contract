d117692f504eaca3f8fb14779454bd8d9b607d72
alihaider123go <alihaider123go@gmail.com>
2024-07-12 16:18:28 +0500

    deploy file change

--- deploy.js
+++ deploy.js
@@ -20,12 +20,12 @@ const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
 
   console.log('Attempting to deploy from account', accounts[0]);
-
+  console.log(interface);
   const result = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode })
     .send({ gas: '1000000', from: accounts[0] });
 
-  console.log(interface);
+  console.log("Contract deployed to", result);
   console.log("Contract deployed to", result.options.address);
   provider.engine.stop();
 };
